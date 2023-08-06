8ef527813397b0183754d3a34e260bbb9cf51314
micnekr <misha.nekrasov@gmail.com>
2023-08-06 19:06:30 +0100

    Fixed nucleus creation

--- common/nuclear.py
+++ common/nuclear.py
@@ -1,5 +1,6 @@
 import manim as _m
 import numpy as np
+import random
 import math
 
 # The different particles
@@ -41,59 +42,61 @@ def create_particle(type, size_multiplier=0.3):
     circle.set_stroke(color[0], opacity=1)
     circle.set_fill(color[1], opacity=1)
     return circle
+
+class Nucleus():
+    # Generates a pattern of circles within circles
+    def _generate_full_nucleus_pattern(number_of_particles, nucleon_separation, particle_num_difference):
+        # The pattern consists of n circles, each with a different number of particles
+        # Let's see how many particles there are in each circle
+        particle_nums = []
+        particle_num_in_layer = 1
+        while True:
+            # If ran out of particles, add the remaining ones
+            if number_of_particles <= particle_num_in_layer:
+                particle_nums.append(number_of_particles)
+                break
+            # Reduce the number of available particles
+            number_of_particles -= particle_num_in_layer
+            particle_nums.append(particle_num_in_layer)
+            
+            
+            particle_num_in_layer += particle_num_difference
     
-# Generates a pattern of circles within circles
-def _generate_full_nucleus_pattern(number_of_particles, nucleon_separation, particle_num_difference):
-    # The pattern consists of n circles, each with a different number of particles
-    # Let's see how many particles there are in each circle
-    particle_nums = []
-    particle_num_in_layer = 1
-    while True:
-        # If ran out of particles, add the remaining ones
-        if number_of_particles <= particle_num_in_layer:
-            particle_nums.append(number_of_particles)
-            break
-        # Reduce the number of available particles
-        number_of_particles -= particle_num_in_layer
-        particle_nums.append(particle_num_in_layer)
-        
-        
-        particle_num_in_layer += particle_num_difference
-
-    out = []
-    # For each of circles, generate particles evenly around the circle, and then displace them
-    for particle_num in particle_nums:
-        # Pick the radius in such a way that the nucleon_separation is the distance between two consequtive nucleons
-        # Arc length is nucleon_separation, and arc length = radius * angle
-        # Each small angle is 2pi/particle_num
-        radius = nucleon_separation / (2*np.pi) * particle_num
-        for angle in np.linspace(0, 2*np.pi, particle_num, endpoint=False):
-            x = radius * np.cos(angle)
-            y = radius * np.sin(angle)
-            out.append((x, y))
-    return out
-
-def create_nucleus(num_protons, num_neutrons, nucleon_separation, particle_num_difference, nucleon_size_multiplier=0.3, seed=1):
-    import random
-    import math
-    random.seed(seed)
-
-    sq_nucleon_separation = nucleon_separation ** 2
-
-    # Make a random list of neutrons and protons
-    nucleon_types = [PROTON] * num_protons + [NEUTRON] * num_neutrons
-    random.shuffle(nucleon_types)
-
-    pattern = _generate_full_nucleus_pattern(num_protons + num_neutrons, nucleon_separation, particle_num_difference)
-    # shuffle the pattern as well to introduce a random z-index to the nucleons (for more interesting overlapping)
-    random.shuffle(pattern)
-
-    out = _m.VGroup()
-    for nucleon_type, position in zip(nucleon_types, pattern):
-        x, y = position
-        proton = create_particle(nucleon_type, nucleon_size_multiplier)
-        proton.shift(x * _m.RIGHT, y * _m.UP)
-        out.add(proton)
-        
-    return out
+        out = []
+        # For each of circles, generate particles evenly around the circle
+        for particle_num in particle_nums:
+            # Pick the radius in such a way that the nucleon_separation is the distance between two consequtive nucleons
+            # Arc length is nucleon_separation, and arc length = radius * angle
+            # Each small angle is 2pi/particle_num
+            radius = nucleon_separation / (2*np.pi) * particle_num
+            for angle in np.linspace(0, 2*np.pi, particle_num, endpoint=False):
+                x = radius * np.cos(angle)
+                y = radius * np.sin(angle)
+                out.append((x, y))
+        return out
     
+    def __init__(self, num_protons, num_neutrons, nucleon_separation, particle_num_difference, nucleon_size_multiplier=0.3, seed=1):
+        super().__init__()
+        self.nucleons = []
+        self.vgroup = _m.VGroup()
+        random.seed(seed)
+    
+        sq_nucleon_separation = nucleon_separation ** 2
+    
+        # Make a random list of neutrons and protons
+        nucleon_types = [PROTON] * num_protons + [NEUTRON] * num_neutrons
+        random.shuffle(nucleon_types)
+    
+        pattern = Nucleus._generate_full_nucleus_pattern(num_protons + num_neutrons, nucleon_separation, particle_num_difference)
+        # shuffle the pattern as well to introduce a random z-index to the nucleons (for more interesting overlapping)
+        random.shuffle(pattern)
+    
+        for nucleon_type, position in zip(nucleon_types, pattern):
+            x, y = position
+            nucleon = create_particle(nucleon_type, nucleon_size_multiplier)
+            nucleon.shift(x * _m.RIGHT, y * _m.UP)
+            self.vgroup.add(nucleon)
+            self.nucleons.append(nucleon)
+
+    def get_create_anims(self):
+        return map(lambda nucleon: _m.Create(nucleon), self.nucleons)
