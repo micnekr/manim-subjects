e47cc8c297bafa07caf83ecd5be59e8b8f088ea4
micnekr <misha.nekrasov@gmail.com>
2023-08-09 12:54:31 +0100

    Added a scene for forces and their ranges

--- physics/3.2.1.2.py
+++ physics/3.2.1.2.py
@@ -71,9 +71,10 @@ class TypesOfDecayScene(Scene):
         gamma_label.add_updater(lambda x: x.move_to(initial_arrow.point_from_proportion(0.7)).shift(0.3 * UP))
         self.add(initial_arrow, gamma_label)
         # Go from each original nucleon to the shuffled one
-        self.play(ReplacementTransform(initial_arrow, final_arrow), 
-                  *map(lambda n: ReplacementTransform(n[0], n[1]),
+        self.play(*map(lambda n: ReplacementTransform(n[0], n[1]),
                        zip(new_nucleons, new_nucleus.get_nucleons_list())))
+        self.wait(1)
+        self.play(ReplacementTransform(initial_arrow, final_arrow))
         self.wait(1)
 
     def beta_decay(self, is_plus, nucleus, heading):
@@ -98,8 +99,8 @@ class TypesOfDecayScene(Scene):
         beta = nuclear.Particle(beta_type, PARTICLE_SIZE_MULTIPLIER)
         beta.move_to(nucleon)
 
-        beta_label1 = nuclear.ParticleLabel(beta, PARTICLE_LABEL_FONT_SIZE, f"\\beta^{sign_letter}")
-        beta_label2 = nuclear.ParticleLabel(beta, PARTICLE_LABEL_FONT_SIZE)
+        beta_label1 = nuclear.ParticleLabel(beta, PARTICLE_LABEL_FONT_SIZE)
+        beta_label2 = nuclear.ParticleLabel(beta, PARTICLE_LABEL_FONT_SIZE, f"\\beta^{sign_letter}")
 
         neutrino = nuclear.Particle(nuclear.NEUTRINO, PARTICLE_SIZE_MULTIPLIER)
         neutrino_label = nuclear.ParticleLabel(neutrino, PARTICLE_LABEL_FONT_SIZE, neutrino_label, color=DARK_GRAY)
@@ -188,27 +189,30 @@ class ForcesHoldingNucleusTogetherScene(Scene):
 # Coefficients for strong force graph
 STRONG_FORCE_A = 2
 STRONG_FORCE_B = -0.95
-STRONG_FORCE_C = -7
-EM_FORCE_A = 0.4
+STRONG_FORCE_C = ValueTracker(-5)
+EM_FORCE_A = ValueTracker(0.7)
+EM_FORCE_B = 0.2
 
-class CompareForces(Scene):
+class CompareForcesScene(Scene):
 
     def strong_force_calculation(x):
         # Do some shifting and scaling to the input
         x = STRONG_FORCE_A * x + STRONG_FORCE_B
-        return STRONG_FORCE_C * x / np.exp(x)
+        return STRONG_FORCE_C.get_value() * x / np.exp(x)
 
     def em_force_calculation(x):
         # Do some shifting and scaling to the input
-        return EM_FORCE_A * x ** -2
+        x -= EM_FORCE_B
+        return EM_FORCE_A.get_value() * x ** -0.8 + 0.3
 
     def construct(self):
         ax = Axes(
-            x_range=[0, 4.6, 0.5],
+            x_range=[0, 4.7, 0.5],
             x_length=6,
             y_length=4,
             x_axis_config={
                 "numbers_to_include": [0.5, 1, 3],
+                "include_ticks": False,
             },
             y_axis_config={
                 "numbers_to_include": [],
@@ -223,19 +227,58 @@ class CompareForces(Scene):
             buff=0.3,
         )
         x_label = ax.get_x_axis_label(
-            Tex("Distance").scale(0.65), edge=DOWN, direction=DOWN, buff=0.5
+            Tex("Distance/fm").scale(0.65), edge=DOWN, direction=DOWN, buff=0.5
         )
-        strong_force_graph = ax.plot(CompareForces.strong_force_calculation, color=GREEN_B, x_range=[0.3, 4, 0.05])
-        em_force_graph = ax.plot(CompareForces.em_force_calculation, color=YELLOW, x_range=[0.3, 4, 0.05])
-        axes_group = VGroup(y_label, x_label, ax)
-        entire_graph = VGroup(axes_group, em_force_graph, strong_force_graph)
+        strong_force_graph_repulsive = always_redraw(lambda: ax.plot(CompareForcesScene.strong_force_calculation, color=GREEN_B, x_range=[0.3, 0.5, 0.05]))
+        strong_force_graph_attractive = always_redraw(lambda: ax.plot(CompareForcesScene.strong_force_calculation, color=GREEN_B, x_range=[0.5, 3, 0.05]))
+        strong_force_graph_negligible = always_redraw(lambda: ax.plot(CompareForcesScene.strong_force_calculation, color=GREEN_B, x_range=[3, 4, 0.05]))
 
-        entire_graph.shift(2 * LEFT)
+        # A graph that is invisible that is used for calculations
+        strong_force_graph_invisible = always_redraw(lambda: ax.plot(CompareForcesScene.strong_force_calculation, color=GREEN_B, x_range=[0.3, 4, 0.05]))
 
-        self.play(Write(axes_group))
+        em_force_graph = always_redraw(lambda: ax.plot(CompareForcesScene.em_force_calculation, color=YELLOW, x_range=[0.3, 4, 0.05]))
+
+        distance = ValueTracker(1)
+
+        entire_graph = VGroup(y_label, x_label, ax, em_force_graph, strong_force_graph_repulsive, strong_force_graph_attractive, strong_force_graph_negligible)
+
+        self.play(Write(y_label), Write(x_label), Write(ax))
         self.wait()
         self.play(Create(em_force_graph))
         self.wait()
-        self.play(Create(strong_force_graph))
+        self.play(Create(strong_force_graph_repulsive))
+        self.wait()
+        self.play(Create(strong_force_graph_attractive))
+        self.wait()
+        self.play(Create(strong_force_graph_negligible))
 
-        self.wait(20)
+        attractive_label = Text("Stable", font_size=50).shift(2 * UP)
+        repulsive_label = Text("Unstable", font_size=50).shift(2 * UP)
+
+        is_attractive_label = attractive_label.copy()
+
+        ## Compare small and large nuclei
+        particle_size_multiplier = 0.4
+        nucleon_separation = 0.6
+        nucleus = nuclear.Nucleus().init_from_nums(3, 3, nucleon_separation, 4, particle_size_multiplier, seed=4)
+        nucleus.shift(3 * RIGHT)
+
+        big_proton_rich_nucleus = nuclear.Nucleus().init_from_nums(5, 4, nucleon_separation, 4, particle_size_multiplier, seed=3).move_to(nucleus)
+        big_neutron_rich_nucleus = nuclear.Nucleus().init_from_nums(5, 6, nucleon_separation, 5, particle_size_multiplier, seed=3).move_to(nucleus)
+
+        self.play(entire_graph.animate.shift(2 * LEFT))
+
+        line_to_em = always_redraw(lambda: ax.get_vertical_line(ax.input_to_graph_point(distance.get_value(), em_force_graph), color=BLUE, line_config={"dashed_ratio": 0.85}))
+        line_to_strong = always_redraw(lambda: ax.get_vertical_line(ax.input_to_graph_point(distance.get_value(), strong_force_graph_invisible), color=BLUE, line_config={"dashed_ratio": 0.85}))
+
+        self.play(*nucleus.create_anims(DrawBorderThenFill), Create(line_to_em), Create(line_to_strong), Write(is_attractive_label))
+        self.wait()
+
+        # Increase the distance by adding more nucleons
+        self.play(EM_FORCE_A.animate.set_value(1.2), STRONG_FORCE_C.animate.set_value(-7), distance.animate.set_value(2), Transform(nucleus, big_proton_rich_nucleus), Transform(is_attractive_label, repulsive_label))
+        self.wait()
+
+        # Increase the strong nuclear force by adding more neutrons
+        self.play(STRONG_FORCE_C.animate.set_value(-10), Transform(nucleus, big_neutron_rich_nucleus), Transform(is_attractive_label, attractive_label))
+
+        self.wait()
